a6712441d54b987431a190628a8a03b03405a4d4
tejas0258 <tj.08032007@gmail.com>
2021-09-07 17:10:28 +0530

    made changes to the code and wrote some comments to make the code readable

--- app.py
+++ app.py
@@ -1,62 +1,87 @@
+# This library gives the color value of each pixel
 from PIL import Image
-from RpiMotorLib import RpiMotorLib
+
+# This library gives control to the stepper motor and the servo motors
+from RpiMotorLib import RpiMotorLib, rpiservolib
+
+# This library helps in giving delays to stop the code if needed
 import time
 
-gpio_pins = (14,15,18)
-direction = 20
-step = 21
-distance= 80
-im = Image.open('hacksmith.jpg')
-pix = im.load()
+gpio_pins = (14,15,18)  #this variable declares the pins that will control the stepper motor 1
+direction = 20          # this pin controls the direction of motor 1
+step = 21               # this pin controls the step of motor 1
+
+gpio_pins2 = (9,10,11)  #this variable declares the pins that will control the stepper motor 2
+direction2 = 12         # this pin controls the direction of motor 2
+step2 = 13              # this pin controls the step of motor 2
+
+image = Image.open('hacksmith.jpg')   # this variable will open the image and store
+pixel = image.load()                  # this variable loads the image and stores the value of it
+
+servo  = rpiservolib.SG90servo("servoone", 50, 2, 12)
+servo2  = rpiservolib.SG90servo("servotwo", 50, 2, 12)
+servo3  = rpiservolib.SG90servo("servothree", 50, 2, 12)
+servo4  = rpiservolib.SG90servo("servofour", 50, 2, 12)
 
 motor = RpiMotorLib.A4988Nema(direction, step, gpio_pins, "A4988")
+motor1 = RpiMotorLib.A4988Nema(direction2, step2, gpio_pins2, "A4988")
 
-
+#This function will compare the color values of each pixel and returns a value what the color can be 
 def colorComparer(value):
+
     g = value[1]
+
     if g in range(245,265):
         print("Yellow")
-        return "y"
+        return "yellow"
 
     elif g in range(135,155):
         print("Orange")
-        return "o"
+        return "orange"
 
     elif g in range(45,60):
         print("Blue")
-        return "b"
+        return "blue"
 
     elif g in range(-5,15):
         print("Red")
-        return 'r'
+        return 'red'
 
+#This function will move the servos according to the color value of the pixel
+def ServoMover(color):
+
+        if color =='red':
+            servo.servo_move(7, 11, 1, False, .01)    # if the color is red then the servo will move ........ this is left
+            time.sleep(2)
+
+        if color =='yellow':
+            servo.servo_move(7, 11, 1, False, .01)
+            time.sleep(2)
+
+        if color == 'orange':
+            servo.servo_move(7, 11, 1, False, .01)
+            time.sleep(2)
+
+        if color == 'blue':
+            servo.servo_move(7, 11, 1, False, .01)
+            time.sleep(2)
 
 for y in range(59,-1,-1):
+
     for x in range(0, 40, 1):
-        RGBValues = pix[x, y]
-        motor.motor_go(False,"Full", 200, 0.0005, 0.5)
-        time.sleep(1)
-        color = colorComparer(RGBValues)
-        if color == 'r':
-            # open red servo stopper
-            time.sleep(2)
-        elif color == 'b':
-            time.sleep(2)
-            #open blue servo stopper
-        elif color == 'o':
-            time.sleep(2)
-            # open yellow servo stopper
-        elif color == 'y':
-            #open yellow servo stopper
-            time.sleep(2)
+
+        RGBValues = pixel[x, y]                                  # this variable will store the rgb color value of the pixel value given to it
+
+        motor.motor_go(False,"Full", 200, 0.0005, 0.5)           # this function will make the motor 1 run clockwise with full steps and take 200 steps with a 0.0005 second delay between each step 
+        motor1.motor_go(True,"FUll", 200, 0.0005, 0.5)           # this function will make the motor 2 run anti - clockwise with full steps and take 200 steps with a 0.0005 second delay between each step 
+
+        time.sleep(1)                                            # this function will stop the code for 1 second
+
+        color = colorComparer(RGBValues)                         # the variable will store the values sent back by the function created
+
         if x == 39:
+            # if this if statement is true then the the motors will go back to the initial position
             motor.motor_go(True,"Full", 8000, 0.0001 , 0.5)
+            motor1.motor_go(False,"Full", 8000, 0.0001 , 0.5)
             time.sleep(1)
 
-
-
-
-#20 51 116 is supposed to be blue
-#229 146 8 is supposed to be orange
-#252 255 0 is suppopsed to be yellow
-#137 4 23 is supposed to be red
